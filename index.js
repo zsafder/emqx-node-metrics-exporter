6e2aa5b1094fb6fb59d5c211d12fdc963eb8611b
Muhammad.Ahmar06 <ahmar@revolvinggames.com>
2020-04-03 15:07:20 +0500

    eqmx-node-metrics-exporter V2.0

--- index.js
+++ index.js
@@ -3,90 +3,138 @@ const express = require('express');
 const server = express();
 const register = new client.Registry();
 
-client.collectDefaultMetrics({prefix: 'node_', timeout: 5000, register});
- 
+client.collectDefaultMetrics({ prefix: 'node_', timeout: 5000, register });
+var username = 'admin' , password = 'public' , auth = "Basic " + Buffer.from(username + ":" + password).toString("base64");
+
+//var username = `${user}` , password = `${pass}` , auth = "Basic " + Buffer.from(username + ":" + password).toString("base64");
+var request = require('request');
+var _metricsResult = [], _nodesResult = [], metricsData = [], clusterKeys = [], clusterValues = [];
 const endpoint = process.env.EMQX_ENDPOINT; 
-const user = process.env.USERNAME;
-const pass = process.env.PASSWORD;
-const port = process.env.PORT;
-const gaugeList = [];
-
-server.get('/metrics', (req, res) => {
-   queryMetrics()
-   .then(data =>{
-      var jsonObject = JSON.parse(data);
-      var nodeMetrics = jsonObject.data[0].metrics;
-      var keys = Object.keys(nodeMetrics) ;
-      var values = Object.values(nodeMetrics) ;
-      var metrics = [];
-      const regex = /\./g;
-
-      for (let i=0; i<keys.length; i++){       
-         metrics[i] = keys[i].replace(regex, '_');
-      }
-
-      for (let i=0; i < metrics.length; i++){
-         gaugeList[i].set(values[i]);
-      }
-
-   }).then((data)=> {
-      res.set('Content-Type', register.contentType);
-      res.end(register.metrics());
-   }).catch((error) => {
-      console.log("Error:: ", error);
-   });
-});
+const emqxnode = process.env.EMQX_NODE;
+const user = process.env.USERNAME, pass = process.env.PASSWORD, port = process.env.PORT;
+const gaugeList = []
+var metricValues = [];
 
 function queryMetrics() {
-   var username = `${user}` , password = `${pass}` , auth = "Basic " + Buffer.from(username + ":" + password).toString("base64");
-   var request = require('request');
-   var url = `${endpoint}/api/v3/metrics/`;
-
+  var url = `${endpoint}/api/v3/nodes/${emqxnode}/metrics/`;
    const options = {
-       url : url,
-       headers : {
-           "Authorization" : auth
-       }
+      url: url,
+      headers: {
+         "Authorization": auth
+      }
    }
    return new Promise((resolve, reject) => {
-       request.get( options, function(error, response, body) {
-          if (!error && response.statusCode == 200) {
-             resolve(body);
-          }
-          else if (response === undefined) {
-             console.log("Error is http request!");
-          }
-       })
-   });
-}
-
-function registerMetric() {
-   return queryMetrics()
-   .then(data =>{
-      var jsonObject = JSON.parse(data);
-      var nodeMetrics = jsonObject.data[0].metrics;
-      var keys = Object.keys(nodeMetrics) ;
-      var values = Object.values(nodeMetrics) ;
-      var metrics = [];
-      const regex = /\./g;
-
-      for (let i=0; i<keys.length; i++){              
-         metrics[i] = keys[i].replace(regex, '_');
-      }
-      
-      for (let i=0; i < metrics.length; i++){
-         gaugeList[i] = new client.Gauge({
-            name: "node_"+ metrics[i] + "_metric_guage",
-            help: metrics[i]
-         });
-         register.registerMetric(gaugeList[i]);
-         gaugeList[i].set(values[i]);
-      }
-
+      request.get(options, function (error, response, body) {
+         if (!error && response.statusCode == 200) {
+            _metricsResult = JSON.parse(body);
+            resolve(_metricsResult);
+         }
+         else if (response === undefined) {
+            console.log("Error in http request!");
+            reject();
+         }
+      })
    })
+   .then((data) => _metricsResult = data);
 }
 
-registerMetric();
+function queryNodes() {
+   var url = `${endpoint}/api/v3/nodes/${emqxnode}`;
+   const options = {
+      url: url,
+      headers: {
+         "Authorization": auth
+      }
+   }
+   return new Promise((resolve, reject) => {
+      request.get(options, function (error, response, body) {
+         if (!error && response.statusCode == 200) {
+            _nodesResult = JSON.parse(body);
+            _nodesResult = JSON.parse(body);
+            resolve(_nodesResult);
+         }
+         else if (response === undefined) {
+            console.log("Error in http request!");
+            reject();
+         }
+      })
+   })
+   .then((data) => _nodesResult = data);
+}
+
+function combineMetrics() {
+   var metricKeys = [], nodesData = [];
+   const regex = /\./g;
+   var nodeKeys = ["connections", "memory_total", "memory_used", "process_available", "process_used", "node_status"];
+
+   metricsData = _metricsResult.data;
+
+   for (let j = 0; j < (nodeKeys.length); j++) {
+      nodesData.push(parseFloat(_nodesResult.data[nodeKeys[j]])); 
+   }
+      console.log(nodesData)
+   var keys = Object.keys(metricsData);
+
+   metricValues = Object.values(metricsData);
+   for (let i = 0; i < keys.length; i++) {
+      metricKeys[i] = keys[i].replace(regex, '_');
+   }
+
+   if (_nodesResult.data["node_status"] == "Running") {
+    nodesData[nodeKeys.indexOf("node_status")] = 1;
+}
+else {
+    nodesData[nodeKeys.indexOf("node_status")] = 0;
+}
+
+   clusterKeys = [...metricKeys, ...nodeKeys];
+   clusterValues = [...metricValues, ...nodesData];
+}
+
+function guageGenerator() {
+   for (let i = 0; i < clusterKeys.length; i++) {
+      gaugeList[i] = new client.Gauge({
+         name: "node_" + clusterKeys[i] + "_metric_guage",
+         help: clusterKeys[i]
+      });
+      register.registerMetric(gaugeList[i]);
+      gaugeList[i].set(clusterValues[i]);
+   }
+}
+
+function fillGuage() {
+   for (let i = 0; i < clusterKeys.length; i++) {
+      gaugeList[i].set(clusterValues[i]);
+   }
+}
+
+function handleSuccess(res) {
+   res.set('Content-Type', register.contentType);
+   res.end(register.metrics());
+}
+
+function handleFailure(res, error) {
+   res.status(500);
+   res.end(error);
+}
+
+server.get('/metrics', (req, res) => {       // Fetches and sets latest metric-values to Guages
+   queryMetrics()
+      .then(() => queryNodes())
+      .then(() => combineMetrics())
+      .then(() => fillGuage())
+      .then(() => handleSuccess(res))
+      .catch((error) => handleFailure(res, error));
+});
+
+function registerMetric() {         
+   return queryMetrics()
+      .then(() => queryNodes())
+      .then(() => combineMetrics())
+      .then(() => guageGenerator())
+}
+
+registerMetric();       // Registers the metrics for /metrics & /nodes APIs 
 
 console.log('Server listening to ' + `${port}` + ', metrics exposed on /metrics endpoint');
-server.listen(`${port}`); 
+server.listen(8888); 
